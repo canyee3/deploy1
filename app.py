45b806d3fde5c1697ecfc4b2b572db890e52d68b
canyee3 <162451974+canyee3@users.noreply.github.com>
2024-05-15 17:10:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ import cv2
 #import urllib.request
 import numpy as np
 
-st.title("Image Classification of select fruits and vegetables")
-st.header("only orange,banana,apple,tomato,bitter guard, capsicum allowed at present")
-st.text("Upload a clear image of fruit or vegetable ")
+st.title("Image Classification of Apples and Tomatoes")
+st.text("Upload a clear image of an apple or a tomato :>")
 from image_classification import machine_classification 
 uploaded_file = st.file_uploader("Enter image", type=["png","jpeg","jpg"])
 
@@ -18,7 +17,7 @@ if uploaded_file is not None:
     st.image(image, caption='Uploaded image', use_column_width=True)
     st.write("")
     st.write("Classifying...")
-    label = machine_classification(image, 'ml_model.h5')
+    label = machine_classification(image, 'best_models.keras')
     if label>=0 and label<=5:
         st.write("The uploaded item is fresh")
     elif label>=6 and label<=11:
